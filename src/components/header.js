3708680da86ad60cd63d32b4b9c8d3ba1221b1e0
Rice <wendykjhgfdsa@gmail.com>
2018-09-02 18:55:48 +0800

    header component
    add header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
+// import logo from '../images/circle_funghi.svg'
 
 
 //reuse component such as header
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div>
+    <div className="Header">
+      <div className="HeaderGroup">
+        <Link to="/"><img src="https://cl.ly/476408c2f409/circle_funghi.png" width="50" /></Link>
+        {/* <Link to="/"><img src={require('../images/circle_funghi.png')} width="35" /></Link> */}
+        <Link to="/">Works</Link>
+        <Link to="/About">About</Link>
+        <Link to="/">Dribbble</Link>
+        <Link to="/">Medium</Link>
+        <Link to="/">Linkedin</Link>
+        <Link to="/">Twitter</Link>
+      </div>
     </div>
   </div>
 )
