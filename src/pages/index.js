6d4062f5733ac8ace3ecbb51abc71df444485d5b
Rice <wendykjhgfdsa@gmail.com>
2018-09-03 00:24:50 +0800

    button transition
    d

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-      <h1>Designing the New Grab Passenger App</h1>
+      <h1>Designing the New Grab <br />Passenger App</h1>
       <p>This project is focused on redesigning our UX, visual and structure hierarchy to make it a better and more familiar experience for our users, as well as create better transport experience and fulfilling different region and business needs.</p>
       <div className="HeroButton"><Link to="/page-2/">See Story</Link></div>
       </div>
